053b0f3050b03ea516c0e44109a94bad946d1250
Zainab Hammami <zainab.hammami.zh@gmail.com>
2017-04-15 16:06:51 +0300

    angularjs

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var handlers = require('./handlers.js');
-var port = 8000;
+var port = process.env.PORT || 8000
 
 var app = express();
 
@@ -8,6 +8,6 @@ require('./middleware.js')(app, express);
 
 app.get('/api/expedia', handlers.getByDestination);
 
-app.listen(process.env.PORT || port);
-
-console.log('app is listening on ' + port)
+server.listen(port, function() {
+    console.log("App is running on port " + port);
+});
